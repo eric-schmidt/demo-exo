41bc9a71eea5af5c3fe9fbff7d3f8f677aa8b7be
Eric Schmidt <e.ryan.schmidt@gmail.com>
2026-07-23 17:34:13 -0600

    Better align Hero image gradient overlay.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,21 +10,23 @@ export const Hero = ({
 }) => {
   return (
     <section className="container relative">
-      <div className="relative z-10 md:max-w-lg px-10 py-20 md:px-10 md:py-40 bg-gradient-to-r from-black/50 to-transparent">
+      <div className="relative z-10 md:max-w-lg px-10 py-20 md:px-10 md:py-40">
         <h1 className="text-4xl md:text-4xl font-bold tracking-tight drop-shadow-lg mb-4">
           {heading}
         </h1>
         {copy && <div className="text-md lg:text-lg mb-4">{copy}</div>}
       </div>
 
-      <Image
-        className="object-cover rounded-lg"
-        priority={true} // prevent Largest Contentful Paint issues
-        fill={true} // add object fit w/o height/width requirement
-        sizes="(min-width: 1280px) 1024px, (min-width: 780px) calc(90.83vw - 121px), calc(100vw - 96px)"
-        src={backgroundImage}
-        alt={alternativeText}
-      />
+      <div className="absolute inset-0 rounded-lg overflow-hidden before:absolute before:inset-0 before:z-[1] before:bg-gradient-to-r before:from-black/50 before:to-transparent before:to-50% before:content-['']">
+        <Image
+          className="object-cover"
+          priority={true} // prevent Largest Contentful Paint issues
+          fill={true} // add object fit w/o height/width requirement
+          sizes="(min-width: 1280px) 1024px, (min-width: 780px) calc(90.83vw - 121px), calc(100vw - 96px)"
+          src={backgroundImage}
+          alt={alternativeText}
+        />
+      </div>
     </section>
   );
 };
